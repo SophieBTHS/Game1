e34f258576a4c83e68d5991b19612b1875d645a0
SophieHughes <sophieh39@nycstudents.net>
2025-10-30 13:20:19 -0400

    Starting on getScore

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,16 @@
 public class Game {
     private boolean bonus = false;
+    public void makeBonus(){bonus = true;}
+    private Level levelOne = new Level();
+    private Level levelTwo = new Level();
+    private Level levelThree = new Level();
     public boolean isBonus(){
         return bonus;
     }
-
+    public void setup(boolean[] goals, int[] points){
+        leve
+    }
+//    public getScore(){
+//
+//    }
 }
--- src/Level.java
+++ src/Level.java
@@ -1,10 +1,8 @@
 public class Level {
     private boolean goalReached;
     private int points;
-    public Level(boolean goalReached, int points){
-        this.goalReached = goalReached;
-        this.points = points;
-    }
+    public void setGoalReached(boolean goalReached){this.goalReached = goalReached;}
+    public void setPoints(int points){this.points = points;}
     public boolean goalReached(){
         return goalReached;
     }
