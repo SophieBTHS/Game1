0773a07d4be30662e3a5f967d81315ff5dd4dec5
SophieHughes <sophieh39@nycstudents.net>
2025-11-06 13:06:29 -0500

    Finsihed with part A!!!!

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,5 @@
 public class Game {
     private boolean bonus = false;
-    public void makeBonus(){bonus = true;}
     private Level levelOne = new Level();
     private Level levelTwo = new Level();
     private Level levelThree = new Level();
@@ -8,21 +7,22 @@ public class Game {
     public boolean isBonus(){
         return bonus;
     }
-    public void setup(boolean[] goals, int[] points){
+    public void setup(boolean[] goals, int[] points, boolean bonus){
         for(int i = 0; i<3;i++){
             levels[i].setGoalReached(goals[i]);
             levels[i].setPoints(points[i]);
         }
-
+        this.bonus = bonus;
     }
     public int getScore(){
         int points = 0;
         for(int i = 0;i<3;i++){
+            int levelPoints = levels[i].getPoints();
             if(levels[i].goalReached()){
-                points += levels[i].getPoints();
-                if (bonus){
-                    points *= 3;
+                if (this.isBonus()){
+                    levelPoints *= 3;
                 }
+                points += levelPoints;
             }
             else{
                 return points;
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 public class Main {
-    Game g1 = new Game();
+    public static void main(String[] args) {
+        Game g = new Game();
+        boolean[] totalGoals = {true, true, true, true, true, true, false, false, true, false, true, true, false, true, true, false};
+        int[] points = {200, 100, 500};
+        for(int i = 0;i<16; i+=4){
+            boolean[] goals = {totalGoals[i], totalGoals[i+1], totalGoals[i+2]};
+            g.setup(goals, points, totalGoals[i+3]);
+            System.out.println(g.getScore());
+        }
+    }
 
 }
